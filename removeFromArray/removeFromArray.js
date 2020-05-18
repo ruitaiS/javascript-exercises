c6ae508368d0862526c4c847f56638d07025c7fd
ruitaiS <shaoruitai@gmail.com>
2020-05-18 19:47:27 -0400

    finished removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+
+function swap(arr, i1, i2){
 
 }
+
+function removeEl(arr, el){
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i]===el){
+            arr.splice(i, 1);
+        };
+    };
+};
+    
+    for (let i = 1; i <arguments.length; i++){
+        removeEl(arr, arguments[i]);
+    };
+
+    return arr; 
+
+};
 
 module.exports = removeFromArray
