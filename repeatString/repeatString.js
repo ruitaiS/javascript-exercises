868e54ad1e69918439bd3b01220ddb096c57d357
ruitaiS <shaoruitai@gmail.com>
2020-05-18 18:59:02 -0400

    Completed repeatstring

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+    if (times < 0){
+        return "ERROR";
+    }else{
+        res = "";
+        for (let i = 0; i < times; i++){
+            res += str;
+        };
+        return res;
+    };
+    
 
 }
 
