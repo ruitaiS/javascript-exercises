4b7527b915cdb466268747259d1a76b88a8c57e5
ruitaiS <shaoruitai@gmail.com>
2020-05-18 19:12:29 -0400

    finished reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    res = "";
+    for (let i = str.length; i > 0; i --){
+        res += str.charAt(i-1);
+    }
+    return res;
 }
 
 module.exports = reverseString
