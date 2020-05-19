d50566386738fd6237879d3faf49ae257587af6f
ruitaiS <shaoruitai@gmail.com>
2020-05-19 18:56:49 -0400

    finished fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    
+    //cast to number
+    n = n/1;
 
-}
+
+    if (n <= 0){
+        return "OOPS";
+    }
+    else if (n == 1){
+        return 1;
+    }else if (n == 2){
+        return 1;
+    }else{
+        return fibonacci(n-1) + fibonacci(n -2);
+    };
+
+};
 
 module.exports = fibonacci
