c736c9d472e02c6050b5e9ca3d57a3af948df2ae
ruitaiS <shaoruitai@gmail.com>
2020-05-19 15:41:45 -0400

    finished palindromes. Still need to learn regex @ some point.. Also string.replace doesn't change the original string; note this

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function() {
+const palindromes = function(str) {
+    
+    //copied from Stack Overflow tbh...
+    //not super familiar with regex yet
+    str = str.replace(/(~|`|!|@|#|$|%|^|&|\*|\(|\)|{|}|\[|\]|;|:|\"|'|<|,|\.|>|\?|\/|\\|\||-|_|\+|=| )/g,"").toLowerCase();
 
-}
+    arr = str.split("");
+    res = "";
+    while (arr.length > 0){
+        res += arr.pop();
+    }; 
+
+    return (res == str);
+
+};
 
 module.exports = palindromes
