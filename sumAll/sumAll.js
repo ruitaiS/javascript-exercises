0cbe4625d790a237404a78e65f8b010575c8b070
ruitaiS <shaoruitai@gmail.com>
2020-05-18 20:06:27 -0400

    finished sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
-}
+function recSum(x, y){
+    if (x==y){
+        return x;
+    }else{
+        return x + recSum(x+1, y);
+    };
+};
+
+
+    if (!((typeof a == "number")&&(typeof b == "number"))||((a<0)||(b<0))){
+        return "ERROR";
+    }else{
+        let smaller; let bigger;
+        if (a<b){
+            smaller = a;
+            bigger = b;
+        }else{
+            smaller = b;
+            bigger = a;
+        };
+        return recSum(smaller, bigger);
+    };
+
+
+};
 
 module.exports = sumAll
