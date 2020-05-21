879c2ee38c3eb355a1d9c3cf990c519d89e0bb52
ruitaiS <shaoruitai@gmail.com>
2020-05-21 16:28:30 -0400

    working on pigLatin

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,5 +1,13 @@
-function translate() {
-	// body...
+function piggify(word){
+    
+
+};
+
+function translate(phrase) {
+	//break phrase into words, reduce, & return
+    let wordArr = phrase.split(" ");
+    return wordArr.reduce(function (acc, word){ return acc + piggify(word)}, "")
+    
 }
 
 
