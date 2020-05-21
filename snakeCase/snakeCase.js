a77cfa8027981dee68e40bcdcbbae3a9d604f4f6
ruitaiS <shaoruitai@gmail.com>
2020-05-21 14:56:57 -0400

    brain too tiny

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -3,12 +3,15 @@ const snakeCase = function(str) {
     //switch spaces, dashes with underscore
     str = str.replace(/( |-|(\.\.)+)/g, "_");
 
+    //remove underscore at end of string
+    str = str.replace(/$_/g, "");
+
     //remove (most) punctuation (not spaces, dashes, or underscores)
     str = str.replace(/(~|`|!|@|#|$|%|^|&|\*|\(|\)|{|}|\[|\]|;|:|\"|'|<|,|\.|>|\?|\/|\\|\||\+|=)/g,"");
     
     //convert to all lowercase if it isn't camelcase
     //technically this regex only works for two word camelcase
-    if (!(/[a-z]+[A-Z][a-z]+/.test(str))){
+    if (!(/^[a-z]+[A-Z][a-z]+/.test(str))){
         str = str.toLowerCase();
     };
     //else insert underscore before the capital
