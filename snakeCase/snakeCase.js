c4062f2a2b6004d07520c1256b9eca00a5168a05
ruitaiS <shaoruitai@gmail.com>
2020-05-21 01:01:38 -0400

    working on snakecase; trying to figure out regex so it only matches camelcase and not others

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,4 +1,17 @@
-const snakeCase = function() {
+const snakeCase = function(str) {
+
+    //remove (most) punctuation (not spaces, dashes, or underscores)
+    str = str.replace(/(~|`|!|@|#|$|%|^|&|\*|\(|\)|{|}|\[|\]|;|:|\"|'|<|,|\.|>|\?|\/|\\|\||\+|=)/g,"");
+    //switch spaces, dashes with underscore
+    str = str.replace(/( |-)/g, "_");
+    
+    //convert to all lowercase if it isn't camelcase
+    //technically this regex only works for two word camelcase
+    if (!(/[a-z]*[A-Z][a-z]*/.test(str))){
+        str.toLowerCase()
+    };
+    return str;
+
 
 }
 
