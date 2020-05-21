90ea88b9db2312c580aebf2a97db429b1a304e56
ruitaiS <shaoruitai@gmail.com>
2020-05-21 17:22:47 -0400

    finished pigLatin! that was fun :)

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,12 +1,14 @@
-function piggify(word){
-    
+function piggify(word){    
+    let post = "";
+    return word.replace(/^((qu)|[^aeiouy])+/, function (pre){post = pre; return "";}) + post + "ay";
 
 };
 
 function translate(phrase) {
-	//break phrase into words, reduce, & return
+	//break phrase into words, reduce, & return after stripping trailing space
     let wordArr = phrase.split(" ");
-    return wordArr.reduce(function (acc, word){ return acc + piggify(word)}, "")
+    return wordArr.reduce(function (acc, word){ return acc + piggify(word) + " ";}, "").replace(/ $/,"");
+
     
 }
 
